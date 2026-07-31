dae76981dbee1bb5fe64e80da81cb2d727a1fe4e
Adam Koehler <adamlanekoehler@gmail.com>
2026-07-30 22:59:12 -0500

    Initial project setup and package upgrades.

--- Dotify.AppHost/AppHost.cs
+++ Dotify.AppHost/AppHost.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var server = builder.AddProject<Projects.Dotify_Server>("server")
+var server = builder.AddProject<Projects.Dotify_API>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
